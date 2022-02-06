e96fbdd2ba15ca43b33805b85f484be789f54af9
Lukas S. | PrimeAPI <primeapi@lukasschmidt.eu>
2022-02-06 16:07:06 +0100

    implemented new restapi

--- src/main/java/de/primeapi/primeplugins/spigotapi/managers/rest/PluginInfo.java
+++ src/main/java/de/primeapi/primeplugins/spigotapi/managers/rest/PluginInfo.java
@@ -13,22 +13,20 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 public class PluginInfo {
-    int code;
-    String message;
+
     int id;
     String name;
+    String latest;
     String displayname;
-    String version;
-    String updateMessage;
-    Long updateTime;
 
     public boolean isNeverVersion(String olderVersion) {
-        String[] curr = olderVersion.split("\\.");
-        String[] neww = getVersion().split("\\.");
+        String[] curr = olderVersion.split("[._]");
+        String[] neww = getLatest().split("[._]");
         for (int i = 0; i < curr.length; i++) {
             if (neww.length > i) {
-                int oldI = Integer.parseInt(curr[i]);
-                int newI = Integer.parseInt(neww[i]);
+                try {
+                    int oldI = Integer.parseInt(curr[i]);
+                    int newI = Integer.parseInt(neww[i]);
                 if (newI > oldI) {
                     return true;
                 } else if (newI == oldI) {
@@ -36,10 +34,16 @@ public class PluginInfo {
                 } else {
                     return false;
                 }
+                }catch (Exception EX){continue;}
             }
         }
 
         return false;
     }
 
+    @Override
+    public String toString() {
+        return "PluginInfo{" + "id=" + id + ", name='" + name + '\'' + ", latest='" + latest + '\'' + ", displayname" +
+                "='" + displayname + '\'' + '}';
+    }
 }
