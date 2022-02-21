51631a398fa6571a95aef218409425f83cd5af34
Exceptionpilot <drach.jonas@web.de>
2022-02-21 21:21:29 +0100

    (remove) Debugs |-> VaultPermission

--- src/main/java/de/primeapi/primeplugins/spigotapi/managers/vault/VaultPermission.java
+++ src/main/java/de/primeapi/primeplugins/spigotapi/managers/vault/VaultPermission.java
@@ -38,79 +38,66 @@ public class VaultPermission extends Permission {
     public boolean playerHas(String world, String playername, String permission) {
         Player p = Bukkit.getPlayer(playername);
         boolean b = PermsAPI.getInstance().hasSelfPermission(p.getUniqueId(), permission).complete();
-        System.out.println("VaultPermission.playerHas: " + b);
         return b;
     }
 
     @Override
     public boolean playerAdd(String s, String s1, String s2) {
-        System.out.println("VaultPermission.playerAdd");
         return false;
     }
 
     @Override
     public boolean playerRemove(String s, String s1, String s2) {
-        System.out.println("VaultPermission.playerRemove");
         return false;
     }
 
     @Override
     public boolean groupHas(String s, String s1, String s2) {
-        System.out.println("VaultPermission.groupHas");
         return false;
     }
 
     @Override
     public boolean groupAdd(String s, String s1, String s2) {
-        System.out.println("VaultPermission.groupAdd");
         return false;
     }
 
     @Override
     public boolean groupRemove(String s, String s1, String s2) {
-        System.out.println("VaultPermission.groupRemove");
         return false;
     }
 
     @Override
     public boolean playerInGroup(String s, String s1, String s2) {
-        System.out.println("VaultPermission.playerInGroup");
         return false;
     }
 
     @Override
     public boolean playerAddGroup(String s, String s1, String s2) {
-        System.out.println("VaultPermission.playerAddGroup");
         return false;
     }
 
     @Override
     public boolean playerRemoveGroup(String s, String s1, String s2) {
-        System.out.println("VaultPermission.playerRemoveGroup");
         return false;
     }
 
     @Override
     public String[] getPlayerGroups(String s, String s1) {
-        System.out.println("VaultPermission.getPlayerGroups");
         return new String[0];
     }
 
     @Override
     public String getPrimaryGroup(String s, String s1) {
-        System.out.println("VaultPermission.getPrimaryGroup");
         return null;
     }
 
     @Override
     public String[] getGroups() {
-        System.out.println("VaultPermission.getGroups");
         return new String[0];
     }
 
     @Override
     public boolean hasGroupSupport() {
-        System.out.println("VaultPermission.hasGroupSupport");
         return false;
     }
 }
