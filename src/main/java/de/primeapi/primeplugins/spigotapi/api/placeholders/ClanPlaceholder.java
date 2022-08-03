2f03e81294db857b51cabf95346ca83cbcee2db2
Exceptionpilot <drach.jonas@web.de>
2022-08-03 12:03:54 +0200

    (change) ClanPlaceholder |-> add %color% placeholder

--- src/main/java/de/primeapi/primeplugins/spigotapi/api/placeholders/ClanPlaceholder.java
+++ src/main/java/de/primeapi/primeplugins/spigotapi/api/placeholders/ClanPlaceholder.java
@@ -15,52 +15,54 @@ import org.bukkit.entity.Player;
  * crated for PrimePlugins
  */
 public class ClanPlaceholder extends PlaceholderExpansion {
-	@Override
-	public String getIdentifier() {
-		return "clan";
-	}
+    @Override
+    public String getIdentifier() {
+        return "clan";
+    }
 
-	@Override
-	public String getAuthor() {
-		return "PrimeAPI";
-	}
+    @Override
+    public String getAuthor() {
+        return "PrimeAPI";
+    }
 
-	@Override
-	public String getVersion() {
-		return PrimeCore.getInstance().getDescription().getVersion();
-	}
+    @Override
+    public String getVersion() {
+        return PrimeCore.getInstance().getDescription().getVersion();
+    }
 
 
-	@Override
-	public String onPlaceholderRequest(Player player, String params) {
-		if (ClanAPI.getInstance().isOnline()) {
-			SQLClan clan = ClanAPI.getInstance().getClanFromPlayer(PrimePlayer.fromPlayer(player)).complete();
-			if (clan == null) {
-				if (params.equalsIgnoreCase("tag_formatted")) {
-					return CoreConfig.getInstance().getString("clanplaceholder.formattedTag.noClan");
-				}
-				return CoreMessage.CLANPLACEHOLDER_NOCLAN.getContent();
-			}
-			switch (params.toLowerCase()) {
-				case "name": {
-					return clan.getRealname().complete();
-				}
-				case "tag": {
-					return clan.getTag().complete();
-				}
-				case "tag_formatted": {
-					return CoreConfig.getInstance()
-					                 .getString("clanplaceholder.formattedTag.format")
-					                 .replaceAll("%tag%", clan.getTag().complete());
-				}
-				case "count": {
-					return String.valueOf(clan.getMembers().complete().size());
-				}
-				case "color": {
-					return clan.getColor().complete();
-				}
-			}
-		}
-		return null;
-	}
+    @Override
+    public String onPlaceholderRequest(Player player, String params) {
+        if (ClanAPI.getInstance().isOnline()) {
+            SQLClan clan = ClanAPI.getInstance().getClanFromPlayer(PrimePlayer.fromPlayer(player)).complete();
+            if (clan == null) {
+                if (params.equalsIgnoreCase("tag_formatted")) {
+                    return CoreConfig.getInstance().getString("clanplaceholder.formattedTag.noClan");
+                }
+                return CoreMessage.CLANPLACEHOLDER_NOCLAN.getContent();
+            }
+            switch (params.toLowerCase()) {
+                case "name": {
+                    return clan.getRealname().complete();
+                }
+                case "tag": {
+                    return clan.getTag().complete();
+                }
+                case "tag_formatted": {
+                    return CoreConfig.getInstance()
+                            .getString("clanplaceholder.formattedTag.format")
+                            .replaceAll("%tag%", clan.getTag().complete())
+                            .replaceAll("%color%", clan.getColor().complete())
+                            ;
+                }
+                case "count": {
+                    return String.valueOf(clan.getMembers().complete().size());
+                }
+                case "color": {
+                    return clan.getColor().complete();
+                }
+            }
+        }
+        return null;
+    }
 }
